2e20a24a5031eb7725c96a2fb0c8eedf232e254f
Tezeroth <terence.honeyford@outlook.com>
2023-11-15 18:29:31 +0000

    added javascript file, cheekily added an image and changed the font

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+
+/* iteration using a for loop on an array to then output to console*/
+
+for (var    x = 0;                  /* sets var to 0, or first in the array*/
+            x < topics.length;      /* if x is less than the length of the shapes array then*/
+            x++                     /* iterate by one  ------ this all is supposed to go on oneline*/
+    ) 
+      
+{
+ console.log(topics[x]);
+}
+
+/*
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+*/
